34f1d2a4854a8c34c8292611f9ad0f1328cc6c80
鸡好 <rutin_chicken@163.com>
2025-03-24 22:04:53 +0800

    Update pac.py

--- pac.py
+++ pac.py
@@ -12,15 +12,15 @@ def select_file():
 def clone_attributes():
     first_file = select_file()
     if not first_file:
-        messagebox.showinfo("未选择文件，程序结束。")
+        messagebox.showinfo("提示", "未选择文件，程序结束。")
         sys.exit()
     second_file = select_file()
     try:
         exif_dict = piexif.load(first_file)
         exif_bytes = piexif.dump(exif_dict)
         piexif.insert(exif_bytes, second_file)
-        messagebox.showinfo("属性克隆成功")
+        messagebox.showinfo("提示", "属性克隆成功")
     except Exception as e:
-        messagebox.showerror(f"发生错误: {e}")
+        messagebox.showerror("提示", f"发生错误: {e}")
 
 clone_attributes()
